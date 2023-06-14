c24554ff5c326a311ce646d4b2b9daa3a604bae0
Aung Pyay Thar <132377611+aungpyaythar1998@users.noreply.github.com>
2023-06-15 01:19:14 +0630

    Add a function for creating arbitrary square divs
    Add createThePad() which will create the number of square divs on
    one side of the pad as the given argument.

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+function sketchThePad(e) {
+    e.target.classList.add("sketched");
+}
+
+function createThePad(squarePerSide) {
+    for(let i = 0; i < squarePerSide; i++) {
+        const rollDiv = document.createElement("div");
+        rollDiv.classList.add("roll");
+        for (let j = 0; j < squarePerSide; j++) {
+            const cellDiv = document.createElement("div");
+            cellDiv.classList.add("cell");
+            cellDiv.addEventListener("mouseover", sketchThePad);
+            rollDiv.appendChild(cellDiv);
+        }
+        container.appendChild(rollDiv);
+    }
+}
+
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-for(let i = 0; i < 16; i++) {
-    const rollDiv = document.createElement("div");
-    rollDiv.classList.add("roll");
-    for (let j = 0; j < 16; j++) {
-        const cellDiv = document.createElement("div");
-        cellDiv.classList.add("cell");
-        rollDiv.appendChild(cellDiv);
-    }
-    container.appendChild(rollDiv);
-}
+createThePad(100);
+
+
